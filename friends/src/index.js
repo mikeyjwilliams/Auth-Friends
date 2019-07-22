85f2f362b972757a11268053913be92443d5cc9c
Michael Williams <manicdesigner@gmail.com>
2019-07-22 17:32:13 -0400

    feat(index.js) + import redux => createstore, applyMiddleware, compose + BrowserRouter import + thunk, logger to middleware + wrap app w/ BrowserRouter

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import {createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import reducer from './reducer';
+import './index.css';
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  compose(
+    applyMiddleware(thunk, logger),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : f => f
+  )
+);
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, document.getElementById('root'));
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
