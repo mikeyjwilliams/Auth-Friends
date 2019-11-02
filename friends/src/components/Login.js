030f1760ac751631629c6a013e8f8d483fa285a0
Michael Williams <manicdesigner@gmail.com>
2019-11-02 13:03:49 -0400

    add input to Login

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,9 +1,21 @@
 import React, { useState } from 'react';
 
 function Login(props) {
+  const [userData, setUserData] = useState({
+    username: '',
+    password: ''
+  });
   return (
     <>
-      <form></form>
+      <form>
+        <label htmlFor='username'>Name:</label>
+        <input
+          type='text'
+          id='username'
+          name='username'
+          placeholder='Username'
+        />{' '}
+      </form>
     </>
   );
 }
